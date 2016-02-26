9499eee0296ce13e094af90b0649eaa2fd6d4793
Iftekhar Khan <iftekharkhan629@gmail.com>
2016-02-26 12:39:29 +0530

    Added the changes for Log Monitoring

---
+++ RallyTestScriptUploader/src/main/java/com/autodesk/rallyuploader/services/MonitoringLogProcesser.java
@@ -0,0 +1,44 @@
+package com.autodesk.rallyuploader.services;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import com.autodesk.rallyuploader.utils.FileUtility;
+
+public class MonitoringLogProcesser {
+	private final String DEFAULT_CONVERSATION_PATTERN = "%d{yyyy-MM-dd HH:mm:ss} [%-4p] [%t] %c:%L - %m%n";
+	private final String DEFAULT_LOG_FILE_PATH = "/opt/specsavers/smsreminder/smsreminder.log";
+	private final String MONITORING_FILE_KEY = "monitoringFile";
+	private final String LEVEL_KEY = "LEVEL";
+	FileAppender fileAppender;
+	FileUtility fileUtility;
+
+	public void setLog4jProperties() {
+		fileUtility = new FileUtility();
+		String monitoringfile = fileUtility.getPropertiesValue(
+				MONITORING_FILE_KEY, DEFAULT_LOG_FILE_PATH);
+		final String LoggingLevel = fileUtility.getPropertiesValue(LEVEL_KEY,
+				DEFAULT_LOG_FILE_PATH);
+		PatternLayout layout = new PatternLayout();
+		String conversionPattern = DEFAULT_CONVERSATION_PATTERN;
+		layout.setConversionPattern(conversionPattern);
+		fileAppender = new FileAppender();
+		fileAppender.setFile(monitoringfile);
+		fileAppender.setLayout(layout);
+		fileAppender.activateOptions();
+		Logger rootLogger = Logger.getRootLogger();
+		if (LoggingLevel.equalsIgnoreCase("ALL"))
+			rootLogger.setLevel(Level.ALL);
+		else if (LoggingLevel.equalsIgnoreCase("DEBUG"))
+			rootLogger.setLevel(Level.DEBUG);
+		else if (LoggingLevel.equalsIgnoreCase("WARN"))
+			rootLogger.setLevel(Level.WARN);
+		else if (LoggingLevel.equalsIgnoreCase("INFO"))
+			rootLogger.setLevel(Level.INFO);
+		else if (LoggingLevel.equalsIgnoreCase("ERROR"))
+			rootLogger.setLevel(Level.ERROR);
+		else
+			rootLogger.setLevel(Level.FATAL);
+		rootLogger.addAppender(fileAppender);
+	}
+}
---
+++ RallyTestScriptUploader/src/main/java/com/autodesk/rallyuploader/utils/FileUtility.java
@@ -0,0 +1,44 @@
+package com.autodesk.rallyuploader.utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+public class FileUtility {
+	static Logger logger = Logger
+			.getLogger(com.autodesk.rallyuploader.utils.FileUtility.class);
+	private BufferedReader bufferedReader;
+	public String convertFileDataToString(String file) throws IOException {
+		 bufferedReader = new BufferedReader(new FileReader(file));
+		String line = null;
+		StringBuilder stringBuilder = new StringBuilder();
+		String ls = System.getProperty("line.separator");
+		while ((line = bufferedReader.readLine()) != null) {
+			stringBuilder.append(line);
+			stringBuilder.append(ls);
+		}
+		return stringBuilder.toString();
+	}
+	public String getPropertiesValue(String key, String filename) {
+		try {
+			File file = new File(filename);
+			FileInputStream fileInput = new FileInputStream(file);
+			Properties properties = new Properties();
+			properties.load(fileInput);
+			String value = properties.getProperty(key);
+			fileInput.close();
+			return value;
+		} catch (FileNotFoundException e) {
+			logger.error("Unable to locate the properties file - " + e);
+		} catch (IOException e) {
+			logger.error("Error while reading the properties file - " + e);
+		}
+		return null;
+	}
+}
