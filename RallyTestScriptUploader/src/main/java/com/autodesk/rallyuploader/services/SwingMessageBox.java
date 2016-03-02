6a7a674450d9959d4746e013cca74b37e0d29f81
Iftekhar Khan <iftekharkhan629@gmail.com>
2016-03-02 18:57:27 +0530

    Added the changes for Splash screen and message box

---
+++ RallyTestScriptUploader/src/main/java/com/autodesk/rallyuploader/services/SwingMessageBox.java
@@ -0,0 +1,43 @@
+package com.autodesk.rallyuploader.services;
+
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class SwingMessageBox {
+	private JFrame frame;
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					/*SwingMessageBox window = new SwingMessageBox();
+					window.frame.setVisible(true);*/
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public SwingMessageBox(String text) {
+		initialize(text);
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize(String text) {
+		Toolkit.getDefaultToolkit().beep();
+	    JOptionPane optionPane = new JOptionPane(text,JOptionPane.INFORMATION_MESSAGE);
+	    JDialog dialog = optionPane.createDialog("Success!");
+	    dialog.setAlwaysOnTop(true);
+	    dialog.setVisible(true);
+	}
+
+}
